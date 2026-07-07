16e261b63967aa0a6810605afa673dcb3a05c7ae
Anjali Lekhwani <anjalilekhwani439@gmail.com>
2026-07-07 18:13:05 +0530

    settings.py changed

--- box_selector/settings.py
+++ box_selector/settings.py
@@ -25,7 +25,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-zdklk8+py@k!q-7cg&9rc(nl+$f5vy=flndk48zhu%c#e8l$by")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
